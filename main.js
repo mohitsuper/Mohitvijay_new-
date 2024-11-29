e3a07f931bc3109efc8d31d356f5aa941341f743
mohitsuper <animemusically6@gamil.com>
2024-11-29 14:24:35 +0530

    mohit

--- main.js
+++ main.js
@@ -1,3 +1,53 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let navLinks = document.querySelectorAll('.a')
+navLinks.forEach((v,i)=>{
+  gsap.from(v,{
+    duration: 1,
+    opacity:0,
+    y:50,
+    scrub:true,
+    ease: 'power2.out',
+  });
+})
+
+gsap.from('#about', {
+  y: 100, 
+  scrollTrigger: {
+      trigger: '#about', 
+      start: 'top 80%', 
+      end: 'bottom 80%', 
+      scrub: true 
+  }
+});
+
+let ServerList = document.querySelectorAll('.service-list');
+ServerList.forEach((v,i)=>{
+  gsap.from(v, {
+    y: 100, 
+    scrollTrigger: {
+        trigger: v, 
+        start: 'top 80%',
+        end: 'bottom 90%', 
+         
+         scrub: true ,
+    }
+  });
+})
+
+let Project  = document.querySelectorAll('.project');
+Project.forEach((v,i)=>{
+  gsap.from(v, {
+    y: 100, 
+    scrollTrigger: {
+        trigger: v, 
+        start: 'top 90%', 
+        end: 'bottom 100%', 
+        scrub: true 
+    }
+  });
+})
+
 
 var Menu = document.querySelector(".menu");
 var Nav = document.querySelector("#sidemenu");
